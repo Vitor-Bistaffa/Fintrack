17f50774f04c057541b1c65af956b4c107f55b7f
Vitor-Bistaffa <VitorBistaffa@gmail.com>
2025-08-03 01:32:26 -0400

    Fix: Nome errados
    Id dos campos estavam errados

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
     { nome: "nome", label: "Nome", tipo: "text" },
     { nome: "descricao", label: "Descrição", tipo: "text" },
     { nome: "valor", label: "Valor", tipo: "number" },
-    { nome: "fk_id_conta", label: "Conta", tipo: "select", lista: "contas" },
-    { nome: "fk_id_categoria", label: "Categoria", tipo: "select", lista: "categorias" },
+    { nome: "conta", label: "Conta", tipo: "select", lista: "contas" },
+    { nome: "categoria", label: "Categoria", tipo: "select", lista: "categorias" },
     { nome: "tipo", label: "Tipo", tipo: "select", lista: "tipos" },
     { nome: "data", label: "Data", tipo: "date" },
     { nome: "parcela", label: "Parcela", tipo: "number" }
